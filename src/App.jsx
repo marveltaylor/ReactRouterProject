94ece69dd628dfbe003f367b27bc5eb4d1e21d5a
marveltaylor <marveltaylor85@gmail.com>
2023-07-29 16:23:18 -0700

    removed h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <div id="container">
-      <h1>Hello React Router!</h1>
       <div id="navbar">{/* navigation here */}</div>
       <div id="main-section">{/* routes here */}</div>
     </div>
