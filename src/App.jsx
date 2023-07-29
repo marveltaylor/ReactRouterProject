32ec378e024197088454e2eb5e6a03cdc26915f7
marveltaylor <marveltaylor85@gmail.com>
2023-07-29 16:55:24 -0700

    created a blue and red link inside navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   return (
     <>
       <div id="container">
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<h1>Blue</h1>}/>
